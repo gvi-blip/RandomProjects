858e594c22837f7472867e01e2ce12a28889e635
Gaurav Virmani <gauravvirmani9175@gmail.com>
2021-09-29 23:56:47 +0530

    Indkluster scraper done

--- scraper.js
+++ scraper.js
@@ -2,9 +2,33 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const fs = require("fs");
 (async () => {
+  const URL = "https://www.indikluster.com/shop";
   const response = await axios.get("https://www.indikluster.com/shop");
   const $ = cheerio.load(response.data);
-  $("h3").each((index, element) => {
-    console.log(index, element.children);
+  let products = [];
+  $("h3").each((index, element) => {//for indkluster products
+    console.log(index, element.children[0].data);
+    products.push(element.children[0].data);
   });
+  //For indkluster products
+  fs.writeFile(
+    "./IndiklusterProducts.txt",
+    ([...products] + "").split(",").join("\n"),
+    (err) => {
+      console.log("Added products to list");
+    }
+    
+    let products = [];
+    $("h3").each((index, element) => {//for marasim products
+      console.log(index, element.children[0].data);
+      products.push(element.children[0].data);
+    });
+    //For marasim products
+  fs.writeFile(
+    "./IndiklusterProducts.txt",
+    ([...products] + "").split(",").join("\n"),
+    (err) => {
+      console.log("Added products to list");
+    }
+  );
 })();
