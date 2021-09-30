40201e8ea1421c45d155bf4638586eea9a0f8797
Gaurav Virmani <gauravvirmani9175@gmail.com>
2021-09-30 19:23:32 +0530

    Indikluster and marasim data stored in text and JSON formats

--- scraper.js
+++ scraper.js
@@ -2,33 +2,47 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const fs = require("fs");
 (async () => {
-  const URL = "https://www.indikluster.com/shop";
-  const response = await axios.get("https://www.indikluster.com/shop");
+  //const URL = "https://www.indikluster.com/shop";
+  const URL = "https://www.marasim.in/all-products?page=4";
+  const response = await axios.get(URL);
   const $ = cheerio.load(response.data);
-  let products = [];
-  $("h3").each((index, element) => {//for indkluster products
-    console.log(index, element.children[0].data);
-    products.push(element.children[0].data);
+
+  //for indkluster products
+  // let products = [];
+  // $("h3").each((index, element) => {
+  //   console.log(index, element.children[0].data);
+  //   products.push(element.children[0].data);
+  // });
+  // //For indkluster products
+  // fs.writeFile(
+  //   "./IndiklusterProducts.txt",
+  //   ([...products] + "").split(",").join("\n"),
+  //   (err) => {
+  //     console.log("Added products to list");
+  //   }
+
+  //for marasim products
+  let data = {
+    brand: "Marasim",
+    products: [],
+  };
+  $("._1bfj5").each((index, element) => {
+    let product = {};
+    product.name = element.children[0].children[0].data; // product names
+    if (element.children[1].children[0].data === "Out of stock") {
+      //console.log(element.children[1].children[0].data); // product availability
+      product.availability = "Out of stock";
+      product.price = null;
+    } else {
+      product.availability = "Available";
+      product.price =
+        element.children[1].children[0].parent.children[1].children[0].data;
+    }
+    data.products.push(product);
   });
-  //For indkluster products
-  fs.writeFile(
-    "./IndiklusterProducts.txt",
-    ([...products] + "").split(",").join("\n"),
-    (err) => {
-      console.log("Added products to list");
-    }
-    
-    let products = [];
-    $("h3").each((index, element) => {//for marasim products
-      console.log(index, element.children[0].data);
-      products.push(element.children[0].data);
-    });
-    //For marasim products
-  fs.writeFile(
-    "./IndiklusterProducts.txt",
-    ([...products] + "").split(",").join("\n"),
-    (err) => {
-      console.log("Added products to list");
-    }
-  );
+
+  //For marasim products
+  fs.writeFile("./marasimProducts.json", JSON.stringify(data), (err) => {
+    console.log("Added products to list");
+  });
 })();
